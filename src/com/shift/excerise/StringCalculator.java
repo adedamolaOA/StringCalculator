53b55c1b1d1babdc2887597c06e3eea3fce9323e
Adedamola <adeleke.adedamola@gmail.com>
2019-08-07 09:01:11 -0600

    Initial commit: create StringCalculator Class and StringCalculator Test Class

---
+++ src/com/shift/excerise/StringCalculator.java
@@ -0,0 +1,12 @@
+package com.shift.excerise;
+
+public class StringCalculator {
+
+    private int add(String numbers){
+        return 0;
+    }
+
+    public static void main(String[] argv){
+
+    }
+}
---
+++ test/com/shift/excerise/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+package com.shift.excerise;
+
+import org.junit.jupiter.api.Test;
+
+class StringCalculatorTest {
+
+    @Test
+    public void add_stringsSeparatedByComma(){
+
+    }
+
+}
