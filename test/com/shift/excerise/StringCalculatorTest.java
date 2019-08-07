3590742c0dd70fba69e03c9d7fde1b624ed5509f
Adedamola <adeleke.adedamola@gmail.com>
2019-08-07 09:26:21 -0600

    Added test case add_stringsSeparatedByComma_handleNewLines()

--- test/com/shift/excerise/StringCalculatorTest.java
+++ test/com/shift/excerise/StringCalculatorTest.java
@@ -31,8 +31,32 @@ class StringCalculatorTest {
     }
 
     @Test
+    public void add_stringsSeparatedByComma_handleNewLines(){
+        int result1 = 0;
+        int result2 = 0;
+        String testNumbers1 = "1\n,2,3";
+        String testNumbers2 = "1,\n2,4";
+        int expected1 = 6;
+        int expected2 = 7;
+        String delimiter =  ",|\n";
+        result1 =  Arrays.asList(testNumbers1.split("([" + delimiter + "])"))
+                .stream()
+                .filter(number -> number.matches("\\d+"))
+                .map(Integer::parseInt)
+                .mapToInt(Integer::intValue).sum();
+        result2 =  Arrays.asList(testNumbers2.split("([" + delimiter + "])"))
+                .stream()
+                .filter(number -> number.matches("\\d+"))
+                .map(Integer::parseInt)
+                .mapToInt(Integer::intValue).sum();
+        assertEquals(expected1, result1);
+        assertEquals(expected2, result2);
+    }
+
+
+    @Test
     public void add_stringsSeparatedByCustomDelimiter(){
-     
+
     }
 
 }
