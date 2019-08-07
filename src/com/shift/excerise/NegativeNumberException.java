ff9c49f3cec8faa8d3e0bd2077a21b444b5d252b
Adedamola <adeleke.adedamola@gmail.com>
2019-08-07 09:52:14 -0600

    Added NegativeNumberException Class to handle negative number in string numbers

---
+++ src/com/shift/excerise/NegativeNumberException.java
@@ -0,0 +1,9 @@
+package com.shift.excerise;
+
+public class NegativeNumberException extends Exception
+{
+    public NegativeNumberException(String message)
+    {
+        super(message);
+    }
+}
